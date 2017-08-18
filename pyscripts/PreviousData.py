3d4c412dab9199ad931f378f6c7d0ca438110daf
ShreyanshLodha <slodha96@gmail.com>
2017-08-18 14:01:48 +0530

    PreviousData.py : Initial script (Not complete)
    Signed-off-by: ShreyanshLodha <slodha96@gmail.com>

---
+++ pyscripts/PreviousData.py
@@ -0,0 +1,42 @@
+import quandl
+import csv
+import authKey
+
+quandl.ApiConfig.api_key = authKey.QuandlKey
+# Output format from Quandl is as follows
+'''
+    Open
+    High
+    Low
+    Close
+    Total Trade Quantity
+    Turnover (Lacs)
+'''
+stock_list = []
+stock_dict = {}
+def dataFetcher():
+    with open('NSE-datasets-codes.csv', newline='') as csvfile :
+        data = csv.reader(csvfile, delimiter = "\n", quotechar="|")
+        for rows in data:
+            apiMaker(rows)
+
+    history()
+
+def apiMaker(rows):
+    temp_list = rows[0].split(',')
+    d = {
+        'stock_code' : temp_list[0],
+        'stock_name' : temp_list[1]
+    }
+    stock_list.append(d.copy())
+
+def history():
+    for items in stock_list:
+        name = str(items['stock_code'])
+        data = quandl.get([name.__add__(".1"),name.__add__(".5")])
+        print(data)
+# todo : choose top 50 with longest duration of data
+# todo : Format the data as per the needs.
+# todo : Insert data into database
+
+dataFetcher()
