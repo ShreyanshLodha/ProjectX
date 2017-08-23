e60afcc3612045ecba9bcd5dd02ea538c4af187f
ShreyanshLodha <slodha96@gmail.com>
2017-08-23 14:52:53 +0530

    Added Login-Register or Dashboard-Logout to nav-bar
    - The script will now make sure that it will not display login on login page and register on signup page
    - The style to UL in every page is necessory to make sure that login-register go to the right side of the page.
    
    Signed-off-by: ShreyanshLodha <slodha96@gmail.com>

--- Project/static/js/cookieChecker.js
+++ Project/static/js/cookieChecker.js
@@ -3,7 +3,6 @@
 
 // Get all the cookies
 var allcookies = document.cookie;
-
 // Split all the cookies with  ';'
 cookiearray = allcookies.split(';');
 
@@ -11,16 +10,26 @@ cookiearray = allcookies.split(';');
 for(var i=0; i<cookiearray.length; i++) {
     name = cookiearray[i].split('=')[0];
     value = cookiearray[i].split('=')[1];
-
     // if a cookie with name 'user-trade' is found then execute if block
     if (name === 'user-trade') {
-        // to modify the content of anchor tag
-        var registerItem = document.getElementById('user-register');
-        registerItem.href = "/dashboard/";
-        registerItem.innerHTML = "Dashboard";
-
-        // if this div exists then hide (reduce chances of errors)
-        if(document.getElementById('user-login'))
-            document.getElementById('user-login').style.display = "none";
+        // to modify the content of anchor tag if the div exists
+        if(document.getElementById('user-register')) {
+            var registerItem = document.getElementById('user-register');
+            registerItem.href = "/dashboard/";
+            registerItem.innerHTML = "Dashboard";
+        }
+        if(document.getElementById('user-login')) {
+            var loginItem = document.getElementById('user-login');
+            loginItem.href = '/logout/';
+            loginItem.innerHTML = 'Logout';
+        }
     }
 }
+
+// To not show login in nav bar if we are on login page
+if((window.location.href).includes("login"))
+    document.getElementById('user-login').style.display="none";
+
+// To not show register in nav bar if we are on sign-up page
+if((window.location.href).includes("signup"))
+    document.getElementById('user-register').style.display="none";
